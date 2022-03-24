afc64331599fb77ff2347cf8149766f7776804af
Roman Dvořák <romandvorak@mlab.cz>
2022-03-24 09:39:42 +0100

    add G-overload FD trigger

--- src/modules/commander/failure_detector/FailureDetector.hpp
+++ src/modules/commander/failure_detector/FailureDetector.hpp
@@ -74,7 +74,9 @@ union failure_detector_status_u {
 		uint16_t arm_escs : 1;
 		uint16_t battery : 1;
 		uint16_t imbalanced_prop : 1;
-		uint16_t in_alt_range: 1;
+		uint16_t att_in_alt_range: 1;
+		uint16_t g_overload: 1;
+		uint16_t goverload_in_alt_range: 1;
 	} flags;
 	uint16_t value {0};
 };
@@ -113,6 +115,11 @@ private:
 	void updateEscsStatus(const vehicle_status_s &vehicle_status, const esc_status_s &esc_status);
 	void updateMotorStatus(const vehicle_status_s &vehicle_status, const esc_status_s &esc_status);
 	void updateImbalancedPropStatus();
+	void updateAccelStatus();
+	void updateAltLimitsStatus();
+
+	bool updateIMUdata();
+	vehicle_imu_status_s _imu_status{};
 
 	failure_detector_status_u _status{};
 
@@ -154,7 +161,9 @@ private:
 		(ParamInt<px4::params::FD_EXT_ATS_TRIG>) _param_fd_ext_ats_trig,
 		(ParamInt<px4::params::FD_ESCS_EN>) _param_escs_en,
 		(ParamInt<px4::params::FD_IMB_PROP_THR>) _param_fd_imb_prop_thr,
-		(ParamFloat<px4::params::FD_ATT_MAX_AGL>) _param_att_max_agl
+		(ParamFloat<px4::params::FD_ATT_MAX_AGL>) _param_fd_att_max_agl,
+		(ParamFloat<px4::params::FD_ACC_MAX_VAL>) _param_fd_acc_max_val,
+		(ParamFloat<px4::params::FD_ACC_MAX_AGL>) _param_fd_acc_max_agl
 
 		// Actuator failure
 		(ParamBool<px4::params::FD_ACT_EN>) _param_fd_actuator_en,

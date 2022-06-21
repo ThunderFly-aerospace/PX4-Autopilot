benodomi/PX4Firmware

add SPS30 driver
working SPS30 driver, connection renew

clean code

src/drivers/drv_sensor.h
@@ -202,6 +202,8 @@
 
 #define DRV_HYGRO_DEVTYPE_SHT3X 0xB1
 
+#define DRV_SENS_DEVTYPE_SPS30 0xB0
+
 #define DRV_DEVTYPE_UNUSED		0xff
 
 #endif /* _DRV_SENSOR_H */

ThunderFly-aerospace/PX4-Autopilot

src/drivers/drv_sensor.h
@@ -213,6 +213,8 @@
 #define DRV_FLOW_DEVTYPE_PX4FLOW 0xB5
 #define DRV_FLOW_DEVTYPE_PAA3905 0xB6
 
+#define DRV_SENS_DEVTYPE_SPS30 0xB7
+
 #define DRV_DEVTYPE_UNUSED		0xff
 
 #endif /* _DRV_SENSOR_H */
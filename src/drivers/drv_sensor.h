benodomi/PX4Firmware

Add support of SEN5x sensors

src/drivers/drv_sensor.h
@@ -202,7 +202,7 @@
 
 #define DRV_HYGRO_DEVTYPE_SHT3X 0xB1
 
-#define DRV_SENS_DEVTYPE_SPS30 0xB0
+#define DRV_SENS_DEVTYPE_SENSIRION_AP 0xB0
 
 #define DRV_DEVTYPE_UNUSED		0xff
 

ThunderFly-aerospace/PX4-Autopilot

src/drivers/drv_sensor.h
@@ -213,7 +213,7 @@
 #define DRV_FLOW_DEVTYPE_PX4FLOW 0xB5
 #define DRV_FLOW_DEVTYPE_PAA3905 0xB6
 
-#define DRV_SENS_DEVTYPE_SPS30 0xB7
+#define DRV_SENS_DEVTYPE_SENSIRION_AP 0xB7
 
 #define DRV_DEVTYPE_UNUSED		0xff
 
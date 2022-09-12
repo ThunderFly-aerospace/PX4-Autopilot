c46de5914816f6bd3fb4c0586a89e6fb8c22fd34
Vit Hanousek <vithanousek@seznam.cz>
2022-09-12 21:23:04 +0200

    platform_open - add parameter of debug value

--- src/modules/platform_open/platform_open.c
+++ src/modules/platform_open/platform_open.c
@@ -3,6 +3,7 @@
 #include <px4_platform_common/posix.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <poll.h>
 #include <string.h>
 #include <math.h>
@@ -16,6 +17,21 @@ int platform_open_main(int argc, char *argv[])
 {
     PX4_INFO("Hello Sky!");
 
+	if(argc!=2)
+	{
+		PX4_ERR("use: platfor_open step_number");
+		return -1;
+	}
+
+	char * pEnd;
+    long int val;
+    val = strtol(argv[1],&pEnd,10);	
+
+	if(pEnd==argv[1])
+	{
+		PX4_ERR("cannot convert param to int");
+		return -1;
+	}
     
     /* advertise attitude topic */
     struct debug_value_s takeoff_information;
@@ -23,7 +39,7 @@ int platform_open_main(int argc, char *argv[])
     orb_advert_t att_pub = orb_advertise(ORB_ID(debug_value), &takeoff_information);
 
 	takeoff_information.timestamp = hrt_absolute_time();
-	takeoff_information.value = 3;
+	takeoff_information.value = val;
 
     orb_publish(ORB_ID(debug_value), att_pub, &takeoff_information);
    

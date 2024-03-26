fc586a81dec1a436908936e8d6265f1e0165f178
Vít Hanousek <vithanousek@seznam.cz>
2024-03-26 17:20:54 +0100

    Log all airborne_practicles topics

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -98,7 +98,6 @@ void LoggedTopics::add_default_topics()
 	add_topic("radio_status");
 	add_topic("rtl_time_estimate", 1000);
 	add_topic("safety");
-	add_topic("sensor_airborne_particles");
 	add_topic("sensor_combined");
 	add_optional_topic("sensor_correction");
 	add_optional_topic("sensor_gyro_fft", 50);
@@ -138,6 +137,7 @@ void LoggedTopics::add_default_topics()
 	add_optional_topic_multi("control_allocator_status", 200, 2);
 	add_optional_topic_multi("rate_ctrl_status", 200, 2);
 	add_optional_topic_multi("sensor_hygrometer", 500, 4);
+	add_optional_topic_multi("sensor_airborne_particles", 500, 4);
 	add_optional_topic_multi("rpm", 200);
 	add_optional_topic_multi("telemetry_status", 1000, 4);
 

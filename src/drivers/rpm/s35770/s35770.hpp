91bb49642674632a8b53a082aa6e3f3b222f00cf
Roman Dvorak <romandvorak@mlab.cz>
2024-08-03 20:49:39 +0200

    s35770 driver fix, nullptr

--- src/drivers/rpm/s35770/s35770.hpp
+++ src/drivers/rpm/s35770/s35770.hpp
@@ -68,12 +68,13 @@ public:
 
 private:
 
-	int  probe() override;
+	int  probe()	override;
 
-	uint32_t       getCounter();
+	uint8_t       getCounter(uint32_t* count);
 
 	uint8_t        hiWord(uint8_t in) { return (in & 0x0fu); }
 	uint8_t        loWord(uint8_t in) { return ((in & 0xf0u) >> 4); }
+
 
 	uint32_t       _count{0};
 	uint16_t       _overflow_count{0};

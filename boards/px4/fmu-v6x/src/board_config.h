2426f8ebcd3cfc2548c7bd04ec7e220bb25fbfd6
Roman Dvorak <romandvorak@mlab.cz>
2024-10-15 18:22:18 +0200

    modifications regarding to TF-G250

--- boards/px4/fmu-v6x/src/board_config.h
+++ boards/px4/fmu-v6x/src/board_config.h
@@ -326,7 +326,7 @@
 /* RC Serial port */
 
 #define RC_SERIAL_PORT                     "/dev/ttyS5"
-#define RC_SERIAL_SINGLEWIRE
+// #define RC_SERIAL_SINGLEWIRE
 
 /* Input Capture Channels. */
 #define INPUT_CAP1_TIMER                  1

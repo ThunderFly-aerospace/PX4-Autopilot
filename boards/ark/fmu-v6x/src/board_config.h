2b8859e8f3fcbc885a926d7c64830f3d1b8aa4e3
Roman Dvorak <romandvorak@mlab.cz>
2024-11-05 12:13:14 +0100

    Create tfg250 board definition

--- boards/ark/fmu-v6x/src/board_config.h
+++ boards/ark/fmu-v6x/src/board_config.h
@@ -385,7 +385,7 @@
  * provides the true logic GPIO BOARD_ADC_xxxx macros.
  */
 #define BOARD_ADC_USB_CONNECTED (px4_arch_gpioread(GPIO_OTGFS_VBUS))
-#define BOARD_ADC_USB_VALID     (!px4_arch_gpioread(GPIO_nVDD_USB_VALID))
+// #define BOARD_ADC_USB_VALID     (!px4_arch_gpioread(GPIO_nVDD_USB_VALID))
 
 /* ARKV6X never powers off the Servo rail */
 

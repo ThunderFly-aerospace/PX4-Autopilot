880f61ff6b10599a23c6fc57062a400de0bfbeb0
Vít Hanousek <vithanousek@seznam.cz>
2025-03-18 14:35:09 +0100

    PX4 modification form TFLora module (more SPI functions, and switch pin on fmuV5)

--- platforms/common/i2c_spi_buses.cpp
+++ platforms/common/i2c_spi_buses.cpp
@@ -647,7 +647,7 @@ int I2CSPIDriverBase::module_start(const BusCLIArguments &cli, BusInstanceIterat
 		I2CSPIDriverBase *instance = initializer_data.instance;
 
 		if (!instance) {
-			PX4_DEBUG("instantiate failed (no device on bus %i (devid 0x%x)?)", iterator.bus(), iterator.devid());
+			PX4_WARN("instantiate failed (no device on bus %i (devid 0x%x)?)", iterator.bus(), (unsigned int)iterator.devid());
 			continue;
 		}
 

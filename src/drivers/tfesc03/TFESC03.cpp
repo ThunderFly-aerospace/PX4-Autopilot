856b7111b2a928f0ea69ca12e9ba6bc3231d1799
Roman Dvorak <romandvorak@mlab.cz>
2025-03-22 23:00:40 +0100

    dummy ESC driver

---
+++ src/drivers/tfesc03/TFESC03.cpp
@@ -0,0 +1,226 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2018-2022 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include "TFESC03.hpp"
+
+#include <px4_platform_common/sem.hpp>
+
+TFESC03::TFESC03(const char *device, uint8_t channels_count) :
+	OutputModuleInterface(MODULE_NAME, px4::serial_port_to_wq(device)),
+	_mixing_output{"TFESC03", channels_count, *this, MixingOutput::SchedulingPolicy::Auto, true},
+	_channels_count(channels_count)
+{
+	//strncpy(_device, device, sizeof(_device) - 1);
+	//_device[sizeof(_device) - 1] = '\0';  // Fix in case of overflow
+
+	_mixing_output.setAllFailsafeValues(0);
+	_mixing_output.setAllDisarmedValues(0);
+	_mixing_output.setAllMinValues(0);
+	_mixing_output.setAllMaxValues(100);
+
+}
+
+TFESC03::~TFESC03()
+{
+	perf_free(_cycle_perf);
+	perf_free(_interval_perf);
+}
+
+int TFESC03::init()
+{
+	PX4_INFO("Initialize I2C device here.");
+	return 0;
+}
+
+void TFESC03::send_esc_outputs(const uint16_t *pwm, const uint8_t motor_cnt)
+{
+	PX4_INFO("Send ESC outputs");
+	for (uint8_t i = 0; i < motor_cnt; i++) {
+		PX4_INFO("Target power for motor %u: %u", i, (unsigned)pwm[i]);
+	}
+	// Placeholder for I2C write
+}
+
+bool TFESC03::updateOutputs(bool stop_motors, uint16_t outputs[8], unsigned num_outputs,
+			    unsigned num_control_groups_updated)
+{
+	PX4_INFO("Update outputs");
+	if (_initialized) {
+		uint16_t motor_out[8] {0};
+
+		for (uint8_t i = 0; i < num_outputs; ++i) {
+			motor_out[i] = outputs[i];
+		}
+
+
+		send_esc_outputs(motor_out, num_outputs);
+
+		// Remove parsing real feedback, set dummy data
+		for (uint8_t i = 0; i < num_outputs; i++) {
+			_esc_feedback.esc[i].timestamp = hrt_absolute_time();
+			_esc_feedback.esc[i].esc_rpm = motor_out[i] * 10; // example dummy speed
+			_esc_feedback.esc[i].esc_voltage = 8.5f;
+			_esc_feedback.esc[i].esc_current = motor_out[i] * 0.01f; // example dummy current
+			_esc_feedback.esc[i].esc_state = 0; // arbitrary
+		}
+		_esc_feedback.esc_count = num_outputs;
+		_esc_feedback.timestamp = hrt_absolute_time();
+		_esc_feedback_pub.publish(_esc_feedback);
+
+		PX4_INFO("Published esc feedback");
+		PX4_INFO("RPM: %u, Voltage: %.2f, Current: %.2f", (unsigned)_esc_feedback.esc[0].esc_rpm,
+			 (double)_esc_feedback.esc[0].esc_voltage, (double)_esc_feedback.esc[0].esc_current);
+
+		return true;
+	}
+
+	return false;
+}
+
+void TFESC03::Run()
+{
+	if (should_exit()) {
+		PX4_INFO("Should exit");
+		ScheduleClear();
+		_mixing_output.unregister();
+
+		exit_and_cleanup();
+		return;
+	}
+
+	// push backup schedule
+	ScheduleDelayed(20_ms);
+
+	perf_begin(_cycle_perf);
+	perf_count(_interval_perf);
+
+	// check for parameter updates
+	if (_parameter_update_sub.updated()) {
+		parameter_update_s pupdate;
+		_parameter_update_sub.copy(&pupdate);
+		updateParams();
+	}
+
+	if (!_initialized) {
+		PX4_INFO("Not initialized");
+		if (init() == PX4_OK) {
+			_initialized = true;
+
+		} else {
+			PX4_ERR("init failed");
+			exit_and_cleanup();
+		}
+
+	} else {
+		_mixing_output.update();
+
+		/* update output status if armed */
+		bool outputs_on;
+		outputs_on = _mixing_output.armed().armed;
+		PX4_INFO("Outputs on: %d", outputs_on);
+
+	}
+
+
+	_mixing_output.updateSubscriptions(true);
+
+	perf_end(_cycle_perf);
+}
+
+int TFESC03::task_spawn(int argc, char *argv[])
+{
+	TFESC03 *instance = new TFESC03(argv[1], (uint8_t)atoi(argv[2]));
+
+	if (instance) {
+		_object.store(instance);
+		_task_id = task_id_is_work_queue;
+
+		if (instance->init() == PX4_OK) {
+			instance->ScheduleNow();
+			return PX4_OK;
+
+		} else {
+			instance->ScheduleClear();
+			_object.store(nullptr);
+			delete instance;
+			return PX4_ERROR;
+		}
+	}
+
+	return PX4_ERROR;
+}
+
+int TFESC03::custom_command(int argc, char *argv[])
+{
+	return print_usage("unknown command");
+}
+
+int TFESC03::print_usage(const char *reason)
+{
+	if (reason) {
+		PX4_INFO("%s\n", reason);
+	}
+
+	PRINT_MODULE_DESCRIPTION(
+		R"DESCR_STR(
+### Description
+Driver for the TFESC03 ESCs.
+
+)DESCR_STR");
+
+	PRINT_MODULE_USAGE_NAME("tfesc03", "driver");
+	PRINT_MODULE_USAGE_COMMAND("start");
+	PRINT_MODULE_USAGE_PARAM_STRING('d', nullptr, "<device>", "Serial device", false);
+	PRINT_MODULE_USAGE_PARAM_INT('c', 4, 1, 8, "Number of channels", false);
+	PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+
+	return 0;
+}
+
+int TFESC03::print_status()
+{
+	PX4_INFO("Running");
+	PX4_INFO("Channels: %u", (unsigned)_channels_count);
+	perf_print_counter(_cycle_perf);
+	perf_print_counter(_interval_perf);
+
+	_mixing_output.printStatus();
+
+	return 0;
+
+}
+
+extern "C" __EXPORT int tfesc03_main(int argc, char *argv[])
+{
+	return TFESC03::main(argc, argv);
+}
+
---
+++ src/drivers/tfesc03/TFESC03.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+#include "TFESC03.hpp"
+
+
+#include <stdint.h>
+
+#include <px4_defines.h>
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/tasks.h>
+#include <px4_platform_common/getopt.h>
+#include <px4_platform_common/posix.h>
+#include <px4_platform_common/module_params.h>
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/getopt.h>
+
+#include <errno.h>
+
+#include <math.h>	// NAN
+#include <cstring>
+
+#include <lib/drivers/device/device.h>
+#include <lib/mixer_module/mixer_module.hpp>
+#include <lib/mathlib/mathlib.h>
+#include <lib/led/led.h>
+#include <lib/tunes/tunes.h>
+
+#include <uORB/PublicationMulti.hpp>
+#include <uORB/Subscription.hpp>
+#include <uORB/topics/esc_status.h>
+#include <uORB/topics/led_control.h>
+
+#include <drivers/drv_hrt.h>
+#include <perf/perf_counter.h>
+
+
+
+
+
+#if !defined(BOARD_TAP_ESC_MODE)
+#  define BOARD_TAP_ESC_MODE 0
+#endif
+
+#if !defined(DEVICE_ARGUMENT_MAX_LENGTH)
+#  define DEVICE_ARGUMENT_MAX_LENGTH 32
+#endif
+
+#define TAP_ESC_MAX_MOTOR_NUM 4
+#define RPMSTOPPED 0
+#define ESC_POS {0, 1, 2, 3}
+#define ESC_DIR {0, 0, 0, 0}
+#define ESC_MAX_RPM 1900
+#define ESC_MIN_RPM 1200
+
+
+using namespace time_literals;
+
+#define MODULE_NAME "tfesc03"
+
+class TFESC03 : public ModuleBase<TFESC03>, public OutputModuleInterface
+{
+public:
+	TFESC03(const char *device, uint8_t channels_count);
+	virtual ~TFESC03();
+
+	static int task_spawn(int argc, char *argv[]);
+	static int custom_command(int argc, char *argv[]);
+	static int print_usage(const char *reason = nullptr);
+	int print_status() override;
+
+	int init();
+	bool updateOutputs(bool stop_motors, uint16_t outputs[8],
+			   unsigned num_outputs, unsigned num_control_groups_updated) override;
+
+private:
+	void Run() override;
+	inline void send_esc_outputs(const uint16_t *pwm, const uint8_t motor_cnt);
+
+	MixingOutput		_mixing_output;
+	perf_counter_t		_cycle_perf{nullptr};
+	perf_counter_t		_interval_perf{nullptr};
+
+	uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update), 1_s};
+
+
+
+	bool _initialized{false};
+	//char _device[DEVICE_ARGUMENT_MAX_LENGTH] {};
+	//int  _uart_fd{-1};
+
+	// const uint8_t _device_mux_map[TAP_ESC_MAX_MOTOR_NUM]  = ESC_POS;
+	// const uint8_t _device_dir_map[TAP_ESC_MAX_MOTOR_NUM]  = ESC_DIR;
+
+	// ESC_UART_BUF _uartbuf{};
+	// EscPacket    _packet{};
+
+	uORB::PublicationMulti<esc_status_s> _esc_feedback_pub{ORB_ID(esc_status)};
+	esc_status_s      _esc_feedback{};
+	uint8_t    	  _channels_count{0}; 		///< number of ESC channels
+	uint8_t 	  _responding_esc{0};
+
+
+	Tunes _tunes{};
+	uORB::Subscription _tune_control_sub{ORB_ID(tune_control)};
+	hrt_abstime _interval_timestamp{0};
+	unsigned int _silence_length{0};	///< If nonzero, silence before next note.
+	unsigned int _frequency{0};
+	unsigned int _duration{0};
+
+	//LedControlData _led_control_data{};
+	//LedController  _led_controller{};
+	//perf_counter_t _interval_perf{perf_alloc(PC_INTERVAL, MODULE_NAME": interval")};
+};

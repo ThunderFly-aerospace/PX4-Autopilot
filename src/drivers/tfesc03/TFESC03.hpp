b67389bb90cd69de78fe65e4b430a0e9d5da4a19
Roman Dvorak <romandvorak@mlab.cz>
2025-04-22 11:17:16 +0200

    TFESC initial commits

--- src/drivers/tfesc03/TFESC03.hpp
+++ src/drivers/tfesc03/TFESC03.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-#include "TFESC03.hpp"
-
-
 #include <stdint.h>
 
 #include <px4_defines.h>
@@ -13,9 +10,9 @@
 #include <px4_platform_common/module_params.h>
 #include <px4_platform_common/px4_config.h>
 #include <px4_platform_common/getopt.h>
+#include <px4_platform_common/i2c_spi_buses.h>
 
 #include <errno.h>
-
 #include <math.h>	// NAN
 #include <cstring>
 
@@ -35,22 +32,13 @@
 
 #include "TFESC03_common.hpp"
 
-
-#if !defined(BOARD_TAP_ESC_MODE)
-#  define BOARD_TAP_ESC_MODE 0
-#endif
-
-#if !defined(DEVICE_ARGUMENT_MAX_LENGTH)
-#  define DEVICE_ARGUMENT_MAX_LENGTH 32
-#endif
-
+// Constants
 #define TAP_ESC_MAX_MOTOR_NUM 4
 #define RPMSTOPPED 0
 #define ESC_POS {0, 1, 2, 3}
 #define ESC_DIR {0, 0, 0, 0}
 #define ESC_MAX_RPM 1900
 #define ESC_MIN_RPM 1200
-
 
 using namespace time_literals;
 
@@ -59,7 +47,7 @@ using namespace time_literals;
 class TFESC03 : public ModuleBase<TFESC03>, public OutputModuleInterface
 {
 public:
-	TFESC03(const char *device, uint8_t channels_count);
+	TFESC03(I2CSPIBusOption bus_option, int bus, int addr, uint8_t channels_count);
 	virtual ~TFESC03();
 
 	static int task_spawn(int argc, char *argv[]);
@@ -75,25 +63,24 @@ private:
 	void Run() override;
 	inline void send_esc_outputs(const uint16_t *pwm, const uint8_t motor_cnt);
 
-	TFESC03_COMMON *_tfesc03_common = nullptr;
+	TFESC03_COMMON *_tfesc03_common{nullptr};
 
-	MixingOutput		_mixing_output;
-	perf_counter_t		_cycle_perf{nullptr};
-	perf_counter_t		_interval_perf{nullptr};
+	MixingOutput _mixing_output;
+	perf_counter_t _cycle_perf{nullptr};
+	perf_counter_t _interval_perf{nullptr};
 
 	uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update), 1_s};
 
 	bool _initialized{false};
 
 	uORB::PublicationMulti<esc_status_s> _esc_feedback_pub{ORB_ID(esc_status)};
-	esc_status_s      _esc_feedback{};
-	uint8_t    	  _channels_count{0}; 		///< number of ESC channels
-	uint8_t 	  _responding_esc{0};
+	esc_status_s _esc_feedback{};
+	uint8_t _channels_count{0};
+	uint8_t _responding_esc{0};
 
 	uORB::Subscription _tune_control_sub{ORB_ID(tune_control)};
 	hrt_abstime _interval_timestamp{0};
-	unsigned int _silence_length{0};	///< If nonzero, silence before next note.
+	unsigned int _silence_length{0};
 	unsigned int _frequency{0};
 	unsigned int _duration{0};
-
 };
